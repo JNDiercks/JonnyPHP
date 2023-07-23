a01368b6734052ce13726debbd2aa43f5dbb35c1
Jan Niklas Diercks <jndiercks@gmail.com>
2023-07-23 19:10:35 +0200

    first commit

---
+++ guestbook.php
@@ -0,0 +1,8 @@
+<html>
+<body>
+
+Welcome <?php echo $_POST["name"]; ?><br>
+Dein Text ist: <?php echo $_POST["guestbook-text"]; ?>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Jonny long schlong</title>
+<link rel="stylesheet" type="text/css" href="./style.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,700,0,0" />
+</head>
+<body>
+<div class="container">
+            <div class="section-welcome section">
+                <div class="flex-center viewheight-full">
+                    <content>
+                    <div class="title-content flex-center">
+                        <div class="welcome-title">
+                            <span class="material-symbols-outlined icon-hover">
+                                sports_tennis
+                            </span>
+                            Jonny's Tennisblog 
+                            <span class="material-symbols-outlined icon-hover">
+                                sports_tennis
+                            </span>
+                        </div>
+                    </div>
+                    <div class="sub-title-content">
+                        Spielberichte, Trainingstagebücher, sexy Fußbilder und vieles mehr...
+                    </div>
+                    </content> 
+                </div>
+            </div>
+            <div class="section-league-game-reports section">
+            </div>
+            <div class="section-training-reports section"></div>
+            <div class="section-tba1 section"></div>
+            <div class="section-tba2 section"></div>
+            <div class="section">
+                <div class="flex-center viewheight-full">
+                    <form action="guestbook.php" method="post">
+                        <p><label for="name">Name:</label></p>
+                        <input type="text" name="name">
+                        <p><label for="guestbook-text">Gästebuch:</label></p>
+                        <textarea id="guestbook-text" name="guestbook-text" rows="4" cols="50"></textarea>
+                        <br>
+                        <input type="submit">
+                    </form>
+                </div>
+
+            </div>
+        </div>
+</body>
+</html>
